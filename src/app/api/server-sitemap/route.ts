4c6919577ccc8323d995ad3fe015516b8303d74d
t-sakamoto <sakamoto051eng@gmail.com>
2024-09-29 19:28:19 +0900

    feat: add article sitemap

--- src/app/api/server-sitemap/route.ts
+++ src/app/api/server-sitemap/route.ts
@@ -8,10 +8,21 @@ export async function GET() {
     distinct: ["isbn"],
   });
 
-  const fields = books.map((book) => ({
-    loc: `https://synapsoul.vercel.app/books/${book.isbn}`,
-    lastmod: new Date().toISOString(),
-  }));
+  // 全ての記事のIDを取得
+  const articles = await db.article.findMany({
+    select: { id: true },
+  });
+
+  const fields = [
+    ...books.map((book) => ({
+      loc: `https://synapsoul.vercel.app/books/${book.isbn}`,
+      lastmod: new Date().toISOString(),
+    })),
+    ...articles.map((article) => ({
+      loc: `https://synapsoul.vercel.app/articles/${article.id}`,
+      lastmod: new Date().toISOString(),
+    })),
+  ];
 
   // getServerSideSitemapをサーバーアクションとして使用
   return getServerSideSitemap(fields);
